8b2c66b005fe63cdda891532f5062d43115bd7f1
Ammar Innowi <ammar.shaukat@innowi.com>
2018-07-30 17:04:16 +0500

    first commit from office

---
+++ Ch5-P7-ConstData/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_P7_ConstData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" const keyword in C# ");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
