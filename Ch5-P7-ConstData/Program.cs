a5bb704d284cab0f60d87f06df063c905d996747
Ammar Innowi <ammar.shaukat@innowi.com>
2018-07-31 09:28:58 +0500

    final commit from offie

--- Ch5-P7-ConstData/Program.cs
+++ Ch5-P7-ConstData/Program.cs
@@ -7,15 +7,73 @@ using System.Threading.Tasks;
 
 namespace Ch5_P7_ConstData
 {
+    class MyMathClass
+    {
+        //public const double PI = 3.14;
+
+
+        // Read-only fields can be assigned in ctors,
+        // but nowhere else.
+
+
+
+        public readonly double PI;
+
+        //public static readonly double PI = 3.14; // static reado-only field
+        static MyMathClass() // Static ctor
+        {
+            //PI = 3.14;
+        }
+        public MyMathClass()
+        {
+            // Not possible- must assign at time of declaration.
+            PI = 3.14;
+        }
+
+
+
+        // Error!
+        public void ChangePI()
+        {
+           // PI = 3.14444;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" const keyword in C# ");
+            Console.WriteLine("***** Fun with Const *****\n");
+
+            //Console.WriteLine("The value of PI is: {0}", MyMathClass.PI);
+
+            //MyMathClass mathClass = new MyMathClass();
+
+            //mathClass.
+
+            // Error! Can't change a constant!
+            //MyMathClass.PI = 3.1444;
 
 
+            //LocalConstStringVariable();
+
+            #region Static Read-Only Fields
+
+            //Console.WriteLine("The value of PI is: {0}", MyMathClass.PI);
+
+            #endregion
 
             Console.ReadLine();
+        }
+
+        private static void LocalConstStringVariable()
+        {
+            // A local constant data point can be directly accessed.
+            const string fixedStr = "Fixed string Data";
+            Console.WriteLine(fixedStr);
+
+
+           // Error!
+           // fixedStr = "This will not work!";
         }
     }
 }
